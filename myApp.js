eb6940584fdf5f16da304fdec70f778e83b5aa61
Arjya Chakraborty <100045241+arjya-007@users.noreply.github.com>
2024-01-13 14:48:20 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
 });
 app.use('/public',express.static(__dirname+'/public'));
 app.get('/json',(req,res) => {
-if(process.env.MESSAGE_STYLE=='uppercase')
+if(process.env.MESSAGE_STYLE=="uppercase")
 res.json({"message":"HELLO JSON"});
 else
 res.json({"message":"hello json"});
